70a31d20861a79bcd0ac4052f475475b683cb84c
jfloresmaquera <jfloresm96@gmail.com>
2021-05-04 16:22:29 -0300

    arreglando tests

--- src/test/java/DuenioTest.java
+++ src/test/java/DuenioTest.java
@@ -15,8 +15,8 @@ public class DuenioTest {
   Mascota bernardo = new Mascota(TipoMascota.PERRO,"Bernardo","Pitoto",5, Sexo.MASCULINO,"Gordo, bajito y tricolor");
   Mascota miguelito = new Mascota(TipoMascota.GATO,"Miguelito","Gato",1,Sexo.MASCULINO,"Gordo y naranja");
 
-  Contacto pepeContacto = new Contacto("Pepe Oscar Mezar",540111587,"pepeMezar@pimientaNegra.com");
-  Duenio unDuenio = new Duenio("Pepe","Chavez",LocalDate.now(),"DNI",19875698,pepeContacto);
+  Contacto pepeContacto = new Contacto("Pepe Oscar",  "Mezar",540111587,"pepeMezar@pimientaNegra.com");
+  Duenio unDuenio = new Duenio("PepeOscar",  "Mezar1234567","Pepe","Chavez",LocalDate.now(),"DNI",19875698,pepeContacto);
 
   @Test
   public void unaMascotaSeRegistraCorrectamenteAUnDuenio(){
